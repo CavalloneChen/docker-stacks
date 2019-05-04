f671feb30c7d9b782f7c54edcb7fb6da7cb11544
Cavallone Chen <chenmaistc@hotmail.com>
2019-05-04 13:20:11 +0800

    fix

--- penv-pytorch/start-notebook.sh
+++ penv-pytorch/start-notebook.sh
@@ -8,7 +8,7 @@ if [[ ! -z "${JUPYTERHUB_API_TOKEN}" ]]; then
   # launched by JupyterHub, use single-user entrypoint
   exec /usr/local/bin/start-singleuser.sh "$@"
 elif [[ ! -z "${JUPYTER_ENABLE_LAB}" ]]; then
-  . /usr/local/bin/start.sh jupyter lab & "$@"
+  . /usr/local/bin/start.sh jupyter lab "$@"
 else
   . /usr/local/bin/start.sh jupyter notebook "$@"
 fi
